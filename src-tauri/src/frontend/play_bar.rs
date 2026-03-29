759d7947e967a626eabf061027697f65bee8bdcc
RomiBrooks <qq1694821929@gmail.com>
2026-03-29 22:39:16 +0800

    small add and fix

---
+++ src-tauri/src/frontend/play_bar.rs
@@ -0,0 +1,7 @@
+pub mod greet;
+pub use greet::push_greet;
+
+pub use greet::push_play;
+pub use greet::push_next;
+pub use greet::push_prev;
+pub use greet::push_status;
--- src-tauri/src/logger/mod.rs
+++
@@ -1,7 +0,0 @@
-// 声明内部子模块
-mod api;
-mod parser;
-mod config;
-
-// 重导出对外接口
-pub use self::api::{init_logger};
